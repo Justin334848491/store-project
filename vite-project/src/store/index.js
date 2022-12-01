4e96b2b5019290cd13a2672c05ddfc6c4f23130c
email <justin.cheng3@student.tdsb.on.ca>
2022-11-30 20:50:02 -0500

    first commit

---
+++ vite-project/src/store/index.js
@@ -0,0 +1,18 @@
+import { defineStore } from 'pinia';
+
+export const indexStore = defineStore("main", {
+    state: () => ({
+        username: "nullPlayer",
+        className: "nullClass",
+        shopingcart: []
+    }),
+    actions: {
+        // setUser(name, className){
+        //     this.username = name;
+        //     this.className = className;
+        }
+    }
+)
+
+// Methods can also be async
+// Learn getters - modify state variables without actually changing them 
---
+++ vite-project/src/views/MainMenu.vue
@@ -0,0 +1,50 @@
+<template>
+    <h1>The Movie Store</h1>
+        <input type="text">
+    </template>
+        <!-- <p class="storyLine"> the </p>
+        <select id="classOptions" ref="classInput">
+            <option>Brute</option>
+            <option>Diseased</option>
+            <option>Thief</option>
+            <option>Fraud</option>
+            <option>Forgotten One</option>
+        </select>
+        <p class="storyLine"> has joined the hunt ...</p>
+        <br>
+        <br>
+        <input type="button" value="Submit your Ticket" @click="setUser()">
+    </div>
+</template> -->
+
+<!-- <script setup>
+import { storeToRefs } from 'pinia';
+import { indexStore } from "../store/index.js";
+import { ref } from 'vue';
+import router from '../router';
+
+const nameInput = ref("");
+const classInput = ref("");
+
+const index = indexStore();
+
+const { username } = storeToRefs(index)
+
+function setUser() {
+    if (nameInput.value.value == null || nameInput.value.value == "") {
+
+    }
+    else {
+        index.setUser(nameInput.value.value, classInput.value.value);
+        router.push("./playerStats");
+    } -->
+<!-- } -->
+
+<!-- </script> -->
+
+<style>
+
+h1 {
+    text-align: center;
+}
+</style>
